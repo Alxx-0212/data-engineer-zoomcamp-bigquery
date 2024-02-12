fba9a999dad13779f8b7d43bb0b840595570d7e3
Alxx-0212 <alexxgo21@gmail.com>
2024-02-12 12:51:06 +0800

    first commit

---
+++ mage/data_exporters/export_to_gcs.py
@@ -0,0 +1,41 @@
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.google_cloud_storage import GoogleCloudStorage
+from pandas import DataFrame
+from os import path
+import os
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/src/project1-366512-29db05379f0d.json'
+
+@data_exporter
+def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
+    """
+    Template for exporting data to a Google Cloud Storage bucket.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#googlecloudstorage
+    """
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    bucket_name = 'green_taxi_tripdata_2022'
+    object_key = 'green_taxi_tripdata_2022'
+    df = df.reset_index(drop=True)
+    table = pa.Table.from_pandas(df)
+    gcs = pa.fs.GcsFileSystem()
+
+    # GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).export(
+    #     df,
+    #     bucket_name,
+    #     object_key,
+    # )
+    pq.write_to_dataset(
+        table,
+        root_path = 'green_taxi_tripdata_2022',
+        filesystem = gcs
+    )
